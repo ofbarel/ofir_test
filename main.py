ca71ff593bde5129ec420898275ea9e29ad1405d
ofbarel <ofir.barel@emedgene.com>
2022-12-06 17:19:36 +0200

    add manual workflow

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import sys
+
+
 def start():
     print("this works")
     return "start finished"
 
 
+def print_args(args):
+    print(f"args : {args}")
+    return {"vals": args[1]}
+
+
 if __name__ == "__main__":
     start()
+    args = sys.argv
+    if len(args) > 1:
+        print_args(args)
